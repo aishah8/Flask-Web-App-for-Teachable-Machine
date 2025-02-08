55e54d5801c4f04ceddcd5abb98ea38242ce31de
aishah8 <aishaa33ahmd@gmail.com>
2025-02-08 13:25:57 +0300

    Add .gitignore to exclude myenv

--- app.py
+++ app.py
@@ -9,18 +9,18 @@ def home():
 
 @app.route('/get_predictions', methods=['GET'])
 def get_predictions():
-    # الاتصال بقاعدة البيانات
+   
     conn = sqlite3.connect('detections.db')
     cursor = conn.cursor()
     
-    # استرجاع البيانات من الجدول
+   
     cursor.execute('SELECT * FROM predictions')
     predictions = cursor.fetchall()
     
-    # غلق الاتصال
+  
     conn.close()
 
-    # إرسال البيانات بشكل منسق للواجهة
+    
     return render_template('predictions.html', predictions=predictions)
 
 if __name__  == '__main__':
--- view_pretdiction.py
+++ view_pretdiction.py
@@ -1,24 +1,24 @@
 import sqlite3
 
-# الاتصال بقاعدة البيانات
+
 conn = sqlite3.connect('detections.db')
 
-# إنشاء مؤشر للتعامل مع البيانات
+
 cursor = conn.cursor()
 
-# إضافة بيانات جديدة
+
 data = [
-    ('iPhone', 0.99),  # iPhone مع الاحتمال 99%
-    ('Laptop', 0.99)   # Laptop مع الاحتمال 99%
+    ('iPhone', 0.99),  
+    ('Laptop', 0.99)   
 ]
 
-# إضافة البيانات إلى الجدول
+
 cursor.executemany("INSERT INTO predictions (label, probability) VALUES (?, ?)", data)
 
-# حفظ التغييرات في قاعدة البيانات
+
 conn.commit()
 
-# غلق الاتصال بقاعدة البيانات
+
 conn.close()
 
 print("تم إضافة البيانات بنجاح: iPhone و Laptop مع التنبؤ 99% لكل منهما")
